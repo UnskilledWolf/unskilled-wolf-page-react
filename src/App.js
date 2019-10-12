1063581f42730f5dffe678536adfb41e2d2c0572
GeroX13 <geronimo.xl13@gmail.com>
2019-10-12 14:58:11 -0700

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, NavLink } from 'react-router-dom';
 
-function App() {
+//*Bootstrap Imports
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="custom" variant="dark" expand="lg">
+        <Navbar.Brand className="mr-3"><NavLink to="/" class="navbar-brand mr-0">Unskilled Wolf</NavLink></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className=""></Nav>
+          <Nav>
+            <NavLink className="nav-link" to="/">Home</NavLink>
+            <NavLink className="nav-link" to="/games">Games</NavLink>
+            <NavLink className="nav-link" to="/software">Software</NavLink>
+            <NavLink className="nav-link" to="/art">Art</NavLink>
+            <NavLink className="nav-link" to="/about">About</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route path="/about"></Route>
+        <Route path="/text"></Route>
+      </Switch>
     </div>
   );
 }
