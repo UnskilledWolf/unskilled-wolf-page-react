4188c60508807db42477a90ce1f648026dd3d9ca
GeroX13 <geronimo.xl13@gmail.com>
2019-10-12 22:00:38 -0700

    Added links to index

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Container from 'react-bootstrap/Container';
+// import Image from 'react-bootstrap/Image';
+// import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom'
 
 export default function HomeSection(props)
 {
@@ -10,15 +11,17 @@ export default function HomeSection(props)
         backgroundImage: `url(${props.image})`
     }
     return (
-        <Row className="homeSection rounded mb-3">
-            <div className="homeGradient" />
-            <Col>
-                <h4>{props.title}</h4>
-                <p>{props.children}</p>
-            </Col>
-            <Col>
-                <div className="paralaxImage rounded" style={IMAGE_STYLE}></div>
-            </Col>
-        </Row>
+        <Link to={props.href} style={{ textDecoration: 'none' }}>
+            <Row className="homeSection rounded mb-3">
+                <div className="homeGradient" />
+                <Col>
+                    <h4>{props.title}</h4>
+                    <p>{props.children}</p>
+                </Col>
+                <Col>
+                    <div className="paralaxImage rounded" style={IMAGE_STYLE}></div>
+                </Col>
+            </Row>
+        </Link>
     )
 }
