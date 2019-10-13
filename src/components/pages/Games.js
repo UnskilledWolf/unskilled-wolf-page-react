c90642b9815b4e5af54b7cd3e6528f6f9b29ce30
GeroX13 <geronimo.xl13@gmail.com>
2019-10-12 21:41:37 -0700

    Added some infromation

--- src/components/pages/Games.js
+++ src/components/pages/Games.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Title from '../Title';
+import Image from 'react-bootstrap/Image';
 
 export default function Games()
 {
@@ -11,7 +12,11 @@ export default function Games()
             <Title>Games</Title>
             <Row>
                 <Col>
-                    <p>I do create games, but most of the are old and bad. There will me new ones someting soon though.</p>
+                    <p>I do create games, but most of the are old and not to my standarts. There will me new ones someting soon though.</p>
+                    <p>I prefer to use Unity3D to create my games, but I have also created some small thing in Processing, and just some plain text things in some other languages.</p>
+                </Col>
+                <Col className="justify-content-center text-center">
+                    <Image src="https://via.placeholder.com/450" />
                 </Col>
             </Row>
         </Container>
--- src/components/pages/Websites.js
+++ src/components/pages/Websites.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Title from '../Title';
+import Image from 'react-bootstrap/Image';
 
 export default function Websites()
 {
@@ -12,9 +13,12 @@ export default function Websites()
             <Row>
                 <Col>
                     <p>
-                        I create websites. I use ReactJS to create them, but I used to use regular HTML, CSS, and JS before.<br />
+                        I create websites. I use ReactJS to create them, but I used to use regular HTML, CSS, and JS before. I also know how do create basci APIs in node, and access databases.<br />
                         I obvioudly created this website myself.
                     </p>
+                </Col>
+                <Col className="justify-content-center text-center">
+                    <Image src="https://via.placeholder.com/450" />
                 </Col>
             </Row>
         </Container>
