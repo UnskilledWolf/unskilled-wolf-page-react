ffdcc1e2664504c404adc420fe8e7608b5451163
Axiom <geronimo.xl13@gmail.com>
2020-12-18 22:23:24 -0800

    Animated Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PARTICLE_PARAMS } from '../particleParams';
 import Particles from 'react-particles-js';
+import { Controller, Scene } from 'react-scrollmagic';
+import { Timeline, Tween } from 'react-gsap';
 
 export interface TitleProps
 {
@@ -17,17 +19,53 @@ class Title extends React.Component<TitleProps, TitleState> {
     render()
     {
         return (
-            <div className="title-container">
-                <div className="title" style={{ backgroundImage: `url(${require("../assets/index_cover_paralax.png")})` }}>
-                    <div className="title-text">
-                        <h1>Unskilled Wolf</h1>
-                        <small>aka Hexagonal193</small>
-                    </div>
-                </div>
-                <div className="title-deco">
-                    <Particles className="particle-footer" params={PARTICLE_PARAMS} />
-                </div>
-            </div>
+            <Controller>
+                <Scene
+                    triggerHook="onLeave"
+                    duration={1000}
+                    pin
+                >
+                    {(progress: any) => (
+                        <div className="title-container">
+                            <Timeline totalProgress={progress} paused>
+                                <div className="title" style={{ backgroundImage: `url(${require("../assets/index_cover_paralax.png")})` }}>
+                                    <Tween
+                                        from={{ left: "0%" }}
+                                        to={{ left: "100%" }}
+                                    >
+                                        <div className="title-deco">
+                                            <div className="scroll-hint">
+                                                <p>
+                                                    Scroll Down <br />
+                                                    <img className="icon" src={require("../assets/scroll-icon.svg")} />
+                                                </p>
+                                            </div>
+                                            <Particles className="particle-footer" params={PARTICLE_PARAMS} />
+                                        </div>
+                                    </Tween>
+                                    <div className="title-text">
+                                        <h1>Unskilled Wolf</h1>
+                                        <small>aka Hexagonal193</small>
+                                    </div>
+                                    <Timeline target={
+                                        <div className="title-extra">
+                                            <ul>
+                                                <li>Art</li>
+                                                <li>Code</li>
+                                                <li>Games</li>
+                                            </ul>
+                                        </div>}>
+                                        <Tween
+                                            from={{ opacity: "0", transform: "translate(-50%,0%)" }}
+                                            to={{ opacity: "1", transform: "translate(-50%,-50%)" }}
+                                        />
+                                    </Timeline>
+                                </div>
+                            </Timeline>
+                        </div>
+                    )}
+                </Scene>
+            </Controller>
         );
     }
 }
