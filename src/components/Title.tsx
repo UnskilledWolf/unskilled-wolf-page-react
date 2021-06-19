f319e1c4b0bd32f11e4062294149562bc0d17e53
Unskilled Wolf <geronimo.xl13@gmail.com>
2021-06-19 13:29:52 +0200

    New Title Screen

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,6 +3,8 @@ import { PARTICLE_PARAMS } from '../particleParams';
 import Particles from 'react-particles-js';
 import { Controller, Scene } from 'react-scrollmagic';
 import { Timeline, Tween } from 'react-gsap';
+import { ReactComponent as LogoSVG } from '../assets/logo.svg'
+import "../logoAnimation.scss"
 
 class Title extends React.Component
 {
@@ -21,33 +23,39 @@ class Title extends React.Component
                                 <div className="title" style={{ backgroundImage: `url(${require("../assets/index_cover_paralax.png")})` }}>
                                     <Tween
                                         from={{ left: "0%" }}
-                                        to={{ left: "100%" }}
+                                        to={{ left: "-75%" }}
                                     >
                                         <div className="title-deco">
+                                            <div className="title-text">
+                                                <Tween
+                                                    from={{ right: "0%", opacity: "1" }}
+                                                    to={{ right: "-50%", opacity: "0" }}>
+                                                    <LogoSVG id="logo" />
+                                                    <h1>Unskilled Wolf</h1>
+                                                    <h2>Axiom</h2>
+                                                </Tween>
+                                            </div>
                                             <div className="scroll-hint">
                                                 <p>
                                                     Scroll Down <br />
                                                     <img className="icon" alt="" src={require("../assets/scroll-icon.svg")} />
                                                 </p>
                                             </div>
-                                            <Particles className="particle-footer" params={PARTICLE_PARAMS} />
+                                            <Particles className="particle-footer" params={PARTICLE_PARAMS} style={{ opacity: "1" }} />
                                         </div>
                                     </Tween>
-                                    <div className="title-text">
-                                        <h1>Unskilled Wolf</h1>
-                                        <small>aka Hexagonal192</small>
-                                    </div>
                                     <Timeline target={
                                         <div className="title-extra">
                                             <ul>
                                                 <li>Art</li>
                                                 <li>Websites</li>
                                                 <li>Code</li>
+                                                <li><small>TODO Graphic Here</small></li>
                                             </ul>
                                         </div>}>
                                         <Tween
-                                            from={{ opacity: "0", transform: "translate(-50%,0%)" }}
-                                            to={{ opacity: "1", transform: "translate(-50%,-50%)" }}
+                                            from={{ opacity: "0", top: "100%" }}
+                                            to={{ opacity: "1", top: "50%" }}
                                         />
                                     </Timeline>
                                 </div>
