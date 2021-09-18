248ef88f5c932c5bf658e4c55179469d6998421d
Unskilled Wolf <geronimo.xl13@gmail.com>
2021-09-18 21:09:05 +0200

    New Icons

--- src/components/QuickNav.tsx
+++ src/components/QuickNav.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import LinkBox from './LinkBox'
 import { SiDeviantart } from 'react-icons/si';
+import { ReactComponent as AboutSVG } from '../assets/Icons_About.svg';
+import { ReactComponent as GallerySVG } from '../assets/Icons_Gallery.svg';
+import { ReactComponent as PlaceholderSVG } from '../assets/Icons_Placeholder.svg'
 
 export default function QuickNav()
 {
@@ -13,14 +16,14 @@ export default function QuickNav()
             </div>
             <div className="links">
                 <div className="collection">
-                    <LinkBox link="/#about" title={<SiDeviantart />} at="About" color="#181818" tcolor="#00a3b9" />
+                    <LinkBox link="/#about" title={<AboutSVG />} at="About" color="#181818" tcolor="#00a3b9" />
+                    <LinkBox link="/gallery" title={<GallerySVG />} at="Gallery" color="#181818" tcolor="#00a3b9" />
                     <LinkBox link="https://www.deviantart.com/hexagonal192" title={<SiDeviantart />} at="Deviantart" color="#00E59B" tcolor="#000000" />
-                    <LinkBox link="/gallery" title={<SiDeviantart />} at="Gallery" color="#181818" tcolor="#00a3b9" />
-                    <LinkBox link="/#" title={<SiDeviantart />} at="Placeholder" color="#181818" tcolor="#00a3b9" />
+                    <LinkBox link="/#" title={<PlaceholderSVG />} at="Placeholder" color="#181818" tcolor="#00a3b9" />
                 </div>
             </div>
             <div className="footer">
-                <p>Quick Nav</p>
+                <p>Quick Navigation</p>
             </div>
         </div>
     )
