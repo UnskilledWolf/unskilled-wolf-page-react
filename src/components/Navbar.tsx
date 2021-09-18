140800fcc41a3450fe458a12b5de8703d744d61f
Axiom <geronimo.xl13@gmail.com>
2021-09-18 16:29:42 +0200

    Navbar with progress indicator

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,54 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export default class NavBar extends Component
+interface INavBarState
 {
+    progress: number,
+    intervalId: number
+}
+
+export default class NavBar extends Component<{}, INavBarState>
+{
+    state = {
+        progress: 0,
+        intervalId: -69
+    }
+
+    getScrollPercent = (): number =>
+    {
+        const h = document.documentElement
+        const b = document.body
+        return (h.scrollTop || b.scrollTop) / ((h.scrollHeight || b.scrollHeight) - h.clientHeight) * 100;
+        //https://stackoverflow.com/a/8028584
+    }
+
+    componentDidMount()
+    {
+        const intervalId = setInterval(() =>
+        {
+            this.setState({ progress: this.getScrollPercent() });
+        }, 10);
+
+        this.setState({ intervalId: intervalId as any })
+    }
+
+    componentWillUnmount()
+    {
+        clearInterval(this.state.intervalId);
+    }
+
     render()
     {
         return (
-            <div className="nav-bar frost-bg">
+            <div className="nav-bar">
+                <div className="progress" style={{ height: `${this.state.progress}%` }} />
                 <nav>
-                    <div className="section">
-                        <Link className="nav-link main" to="/#home">Home</Link>
-                        <a className="nav-link" href="/#art">Art</a>
-                        <a className="nav-link" href="/#websites">Websites</a>
-                        <a className="nav-link" href="/#code">Code</a>
-                        <a className="nav-link" href="/#about">About</a>
-                    </div>
-                    <div className="section">
-                        <Link className="nav-link main" to="/gallery">Gallery</Link>
-                    </div>
+                    <Link className="nav-link main" to="/#home" style={{ top: "0", transform: "translate(50%)" }}>Home</Link>
+                    <a className="nav-link" href="/#art" style={{ top: "51.5%" }}>Art</a>
+                    <a className="nav-link" href="/#websites" style={{ top: "65%" }}>Websites</a>
+                    <a className="nav-link" href="/#code" style={{ top: "78.3%" }}>Code</a>
+                    <a className="nav-link" href="/#about" style={{ top: "93.9%" }}>About</a>
+                    {/* <Link className="nav-link main" to="/gallery">Gallery</Link> */}
                 </nav>
             </div>
         )
