eda156c01ac55f94664c3da3ad73b3075d7c9804
Unskilled Wolf <geronimo.xl13@gmail.com>
2022-08-18 10:29:53 +0200

    Started new TItle page

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { SiDeviantart, SiGithub, SiTwitch } from 'react-icons/si';
+import { GoChevronDown } from 'react-icons/go'
 
 export default function Title()
 {
     return (
         <div className="title">
-            <div className="title-deco">
-                <div className="title-text">
-                    <img src="/assets/logo.svg" alt="Page Logo" />
-                    <h1>Unskilled Wolf</h1>
-                    <h2>Axiom</h2>
+            <div className="logo">
+                <img className="logo-img" src="/assets/logo.svg" />
+                <h1>Unskilled Wolf</h1>
+                <h2>Code, Art, 3D</h2>
+            </div>
+            <div className="links">
+                <div className="links-list">
+                    <ul className='link-icons'>
+                        <li><SiTwitch /></li>
+                        <li><SiDeviantart /></li>
+                        <li><SiGithub /></li>
+                        <li><GoChevronDown /></li>
+                    </ul>
+                    <ul className='link-texts'>
+                        <li><a href="#">Twitch</a></li>
+                        <li><a href="#">DeviantArt</a></li>
+                        <li><a href="#">GitHub</a></li>
+                        <li><a href="#about">About</a></li>
+                    </ul>
                 </div>
-                <div className="scroll-hint">
-                    <a href="/#art">
-                        Scroll Down <br />
-                        <img className="icon" alt="" src={"/assets/scroll-icon.svg"} />
-                    </a>
-                </div>
+            </div>
+            <div className="scroll-hint">
+                <a href="/#art">
+                    Scroll Down <br />
+                    <img className="icon" alt="" src="/assets/scroll-icon.svg" />
+                </a>
             </div>
         </div>
     );
